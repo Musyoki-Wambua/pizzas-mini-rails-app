439ec0e7e73c9ea4865cd43594e85a9d2ec0016f
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-19 10:21:28 +0300

    ActiveRecord Associations

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,4 @@
 class Pizza < ApplicationRecord
+    has_many :restraunt_pizzas
+    has_many :restaurants, through: :restaurant_pizzas
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+    has_many :restaurant_pizzass
+    has_many :pizzas, through: :restaurant_pizzas
 end
