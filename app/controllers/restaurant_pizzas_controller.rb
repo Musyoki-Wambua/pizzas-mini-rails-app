2c1f80c107cd82bc5725b6fd50362f680464bdc7
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-19 13:22:00 +0300

    RestaurantPizza Controller: POST Request

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,3 +1,13 @@
 class RestaurantPizzasController < ApplicationController
-    
+
+    def create 
+        restaurant_pizza = RestaurantPizza.create!(restaurant_pizza_params)
+        render json: restaurant_pizza.pizza, status: :created
+    rescue ActiveRecord::RecordInvalid => e  
+        render json: { error: e.record.errors.full_messages  }, status: :unprocessable_entity
+    end
+
+    def restaurant_pizza_params 
+        params.permit(:price)
+    end
 end
